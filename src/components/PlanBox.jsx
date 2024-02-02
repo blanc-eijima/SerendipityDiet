d48f85b40273bae619dba3751c2974d4fb87f862
blancTo <git@blanc.to>
2024-02-03 06:53:51 +0900

    PayPalリンク追加

--- src/components/PlanBox.jsx
+++ src/components/PlanBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const PlanBox = ({ course, title, children, price }) => {
   const imageUrl = `/images/course_${course}.jpg`;
@@ -6,18 +6,18 @@ const PlanBox = ({ course, title, children, price }) => {
   const courseName = `${course}_title`;
   return (
     <>
-      <div id={course} className='plan_box'>
-        <div className='plan_l'>
+      <div id={course} className="plan_box">
+        <div className="plan_l">
           <h3 className={courseName}>{title}</h3>
           <h4>月額 {price}円</h4>
           {children}
-          <p>
-            <a href='/mousikomi-form/' className='bt01' role='button'>
-              今すぐ申し込む
+          <p className="center">
+            <a href="/mousikomi-form/" className="bt01" role="button">
+              お申し込みフォームから今すぐ申し込む
             </a>
           </p>
         </div>
-        <div className='plan_r'>
+        <div className="plan_r">
           <img src={imageUrl} width={480} height={320} />
         </div>
       </div>
--- src/pages/plan.js
+++ src/pages/plan.js
@@ -1,51 +1,122 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import Section from '../components/Section';
-import PlanBox from '../components/PlanBox';
-import Seo from '../components/Seo';
+/* global paypal */
+import React, { useEffect } from "react";
+import Layout from "../components/Layout";
+import Section from "../components/Section";
+import PlanBox from "../components/PlanBox";
+import Seo from "../components/Seo";
 
 const Plan = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.paypal.com/sdk/js?client-id=AVTYHg0Thk_lq_XB4OK8wk4YpcFj2ZrzeLwXI5q29Jp00W2T7tGnGyk5r-V7VegbBst52HyHIj749Uka&vault=true&intent=subscription";
+    script.addEventListener("load", () => {
+      // シルバーコースのPayPalボタン
+      paypal
+        .Buttons({
+          style: {
+            shape: "rect",
+            color: "gold",
+            layout: "vertical",
+            label: "subscribe",
+          },
+          createSubscription: function (data, actions) {
+            return actions.subscription.create({
+              plan_id: "P-6TT26072SP350245JMW5RCVY",
+            });
+          },
+          onApprove: function (data, actions) {
+            alert(data.subscriptionID);
+          },
+        })
+        .render("#paypal-button-container-P-6TT26072SP350245JMW5RCVY");
+
+      // ゴールドコースのPayPalボタン
+      paypal
+        .Buttons({
+          style: {
+            shape: "rect",
+            color: "gold",
+            layout: "vertical",
+            label: "subscribe",
+          },
+          createSubscription: function (data, actions) {
+            return actions.subscription.create({
+              plan_id: "P-1BH82252997389359MW5SVQA",
+            });
+          },
+          onApprove: function (data, actions) {
+            alert(data.subscriptionID);
+          },
+        })
+        .render("#paypal-button-container-P-1BH82252997389359MW5SVQA");
+
+      // プラチナコースのPayPalボタン
+      paypal
+        .Buttons({
+          style: {
+            shape: "rect",
+            color: "gold",
+            layout: "vertical",
+            label: "subscribe",
+          },
+          createSubscription: function (data, actions) {
+            return actions.subscription.create({
+              plan_id: "P-0J602393Y2089780RMW5S4UY",
+            });
+          },
+          onApprove: function (data, actions) {
+            alert(data.subscriptionID);
+          },
+        })
+        .render("#paypal-button-container-P-0J602393Y2089780RMW5S4UY");
+    });
+    document.body.appendChild(script);
+  }, []);
+
   return (
-    <>
-      <Layout>
-        <Section title='PLAN' sub={true} id='plan' svg={true}>
-          <h1>PLAN</h1>
-          <PlanBox course='silver' title='シルバーコース' price='35,000'>
-            <p>
-              現状をヒヤリングして、
-              <br />
-              翌月の次回コンサルティングまでのプランを提案させていただきます。
-            </p>
-            <p>運動・食生活・生活リズムなどをどのようにしていくかをあなたの経験やライフスタイルに合わせてアドバイスいたします。</p>
-            <p>ビデオ通話（LINE・Skypeなど）、音声通話（固定電話・携帯電話）のどちらでも対応可能です。</p>
-          </PlanBox>
-          <PlanBox course='gold' title='ゴールドコース' price='65,000'>
-            <p>プラン通りにダイエットと進めていったとしても途中で疑問に思うことや、もっとこうしたらいいのではないか？ と思うことが必ず出てくるし、状況も変わってくることが多々あります。</p>
+    <Layout>
+      <Section title="PLAN" sub={true} id="plan" svg={true}>
+        <h1>PLAN</h1>
+        <PlanBox course="silver" title="シルバーコース" price="35,000">
+          <p>
+            現状をヒヤリングして、
+            <br />
+            翌月の次回コンサルティングまでのプランを提案させていただきます。
+          </p>
+          <p>運動・食生活・生活リズムなどをどのようにしていくかをあなたの経験やライフスタイルに合わせてアドバイスいたします。</p>
+          <p>ビデオ通話（LINE・Skypeなど）、音声通話（固定電話・携帯電話）のどちらでも対応可能です。</p>
+          <div className="paypal-bt" id="paypal-button-container-P-6TT26072SP350245JMW5RCVY"></div>
+        </PlanBox>
+        <PlanBox course="gold" title="ゴールドコース" price="65,000">
+          <p>プラン通りにダイエットと進めていったとしても途中で疑問に思うことや、もっとこうしたらいいのではないか？ と思うことが必ず出てくるし、状況も変わってくることが多々あります。</p>
 
-            <p>同じ月にもう１度コンサルティングを行うことによって疑問を解決して、状況に応じたプランを再提案させていただくことができるのでシルバーコースよりも効率的にダイエットを進めていくことができます。</p>
+          <p>同じ月にもう１度コンサルティングを行うことによって疑問を解決して、状況に応じたプランを再提案させていただくことができるのでシルバーコースよりも効率的にダイエットを進めていくことができます。</p>
 
-            <p>シルバーコースと同様でビデオ通話（LINE・Skypeなど）、音声通話（固定電話・携帯電話）のどちらでも対応可能です。</p>
-          </PlanBox>
-          <PlanBox course='platinum' title='プラチナコース' price='95,000'>
-            <p>１ヵ月にコミュニケーションをとる一番最適な回数は３回くらいだと私は考えております。</p>
+          <p>シルバーコースと同様でビデオ通話（LINE・Skypeなど）、音声通話（固定電話・携帯電話）のどちらでも対応可能です。</p>
+          <div className="paypal-bt" id="paypal-button-container-P-1BH82252997389359MW5SVQA"></div>
+        </PlanBox>
+        <PlanBox course="platinum" title="プラチナコース" price="95,000">
+          <p>１ヵ月にコミュニケーションをとる一番最適な回数は３回くらいだと私は考えております。</p>
 
-            <p>コンサルティング回数が多すぎてすぐに質問できる状況だと人は考えなくなってきます。最適なアドバイスを受けることは大事ですが、ご自分で常に考えながら行動していくことも同じくらい大事なのです。</p>
+          <p>コンサルティング回数が多すぎてすぐに質問できる状況だと人は考えなくなってきます。最適なアドバイスを受けることは大事ですが、ご自分で常に考えながら行動していくことも同じくらい大事なのです。</p>
 
-            <p>深く考えながらご自分でも調べて行動していくと新しいことを発見できたり自分の身体のことがわかってくることがよくあります。そうすることによってコンサルティング時に内容の濃い話ができます。ゴールドコースよりも１回コンサルティング回数が増えることによって、更に効率的にダイエットを行うことができてモチベーションが高い状態をキープしながら行動していくことができやすくなります。</p>
+          <p>深く考えながらご自分でも調べて行動していくと新しいことを発見できたり自分の身体のことがわかってくることがよくあります。そうすることによってコンサルティング時に内容の濃い話ができます。ゴールドコースよりも１回コンサルティング回数が増えることによって、更に効率的にダイエットを行うことができてモチベーションが高い状態をキープしながら行動していくことができやすくなります。</p>
 
-            <p>シルバーコース・ゴールドコースと同様でビデオ通話（LINE・Skypeなど）、音声通話（固定電話・携帯電話）のどちらでも対応可能です。</p>
+          <p>シルバーコース・ゴールドコースと同様でビデオ通話（LINE・Skypeなど）、音声通話（固定電話・携帯電話）のどちらでも対応可能です。</p>
 
-            <p>最短で結果へ導くための究極のコースです。</p>
-          </PlanBox>
-        </Section>
-      </Layout>
-    </>
+          <p>最短で結果へ導くための究極のコースです。</p>
+          <div className="paypal-bt" id="paypal-button-container-P-0J602393Y2089780RMW5S4UY"></div>
+        </PlanBox>
+      </Section>
+    </Layout>
   );
 };
+
 export const Head = () => (
   <>
-    <body id='pagetop' />
-    <Seo title='Plan' />
+    <body id="pagetop" />
+    <Seo title="Plan" />
   </>
 );
+
 export default Plan;
