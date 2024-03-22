750c89aa9e27b6a44ab3e60e8d78f0b63cf78347
blancTo <git@blanc.to>
2024-03-23 06:28:16 +0900

    不要タグ削除

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXTwitter, faFacebook, faLine } from "@fortawesome/free-brands-svg-icons";
+import { faXTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import FootSvg from "./FootSvg";
 import { Link } from "gatsby";
 
