336dda7c62c9d4ba53208f9ce9f651301c6f05e6
Kazuhiro Eijima <kazuhiro@blanc.to>
2024-05-29 17:10:15 +0900

    アナリティクス追加

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,22 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: 'ja' });
+import React from "react";
+
+exports.onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+  // 既存のHTML属性設定を保持
+  setHtmlAttributes({ lang: "ja" });
+
+  // Google Analyticsのスクリプトを追加
+  setHeadComponents([
+    <script key="gatsby-gtag-js" async src="https://www.googletagmanager.com/gtag/js?id=G-6TJH5F48C6" />,
+    <script
+      key="gatsby-gtag-init"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6TJH5F48C6');
+        `,
+      }}
+    />,
+  ]);
 };
