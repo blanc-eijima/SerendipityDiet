d52a79596742c2d03a0748ef709a5a9d03be6069
blancTo <git@blanc.to>
2024-10-11 13:00:47 +0900

    ヘッダーロゴの要素をh1→divへ変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "gatsby";
+import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
 
-import Nav from "./Nav";
+import Nav from "./Nav"
 
 const Header = () => {
-  const [navClicked, setNavClicked] = useState(false);
+  const [navClicked, setNavClicked] = useState(false)
 
   const handleNavClick = () => {
-    setNavClicked(!navClicked);
-  };
+    setNavClicked(!navClicked)
+  }
 
   useEffect(() => {
     // navClicked の状態に応じて body のクラスを更新
     if (navClicked) {
-      document.body.classList.add("fixed");
+      document.body.classList.add("fixed")
     } else {
-      document.body.classList.remove("fixed");
+      document.body.classList.remove("fixed")
     }
-  }, [navClicked]);
+  }, [navClicked])
 
   return (
     <>
       <header id="header">
         <div id="header-inner">
-          <h1 className="h_logo">
+          <div className="h_logo">
             <Link to="/">
               <img src="/images/logo.svg" width={200} height={67} className="opa" alt="SERENDIPITY Ultimate Diet" />
             </Link>
-          </h1>
+          </div>
           <Nav navClicked={navClicked} />
           <button className={`navbtn ${navClicked ? "active" : ""}`} onClick={handleNavClick}>
             <span />
@@ -38,7 +38,7 @@ const Header = () => {
         </div>
       </header>
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
